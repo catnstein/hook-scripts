260890504c337463dd4cbee4169a1e9db28041f7
catnstein <mircea.badragan@monsterlynx.com>
2025-12-03 01:42:46 +0200

    feat: file firewall

---
+++ file_firewall/file_firewall.go
@@ -0,0 +1,43 @@
+package filefirewall
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+const (
+	NOT_ALLOWED_ERROR_MESSAGE = "You are not allowed to read this file unfortunately."
+)
+
+func exit() {
+	fmt.Fprintf(os.Stderr, NOT_ALLOWED_ERROR_MESSAGE)
+	os.Exit(2)
+}
+
+func Analyze() {
+	input, err := io.ReadAll(os.Stdin)
+
+	if err != nil {
+		exit()
+	}
+
+	var jsonMap map[string]any
+	if err := json.Unmarshal(input, &jsonMap); err != nil {
+		exit()
+	}
+
+	toolInput, ok := jsonMap["tool_input"].(map[string]any)
+	if ok {
+		filePath := toolInput["file_path"]
+		filePathStr := fmt.Sprintf("%v", filePath)
+		// TODO: proper parsing of the env file
+		if strings.Contains(filePathStr, ".env") {
+			exit()
+		}
+	}
+
+	os.Exit(0)
+}
